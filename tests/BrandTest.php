93957647b3337855d8a84d18753e1a306aded58d
Brett <Moorebrett0@gmail.com>
2015-03-27 10:36:13 -0700

    passing getAll test in brand

--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -70,6 +70,24 @@
             $this->assertEquals($test_brand, $result[0]);
         }
 
+        function test_getAll()
+        {
+            $name = "Sketchers";
+            $id = 1;
+            $name2 = 'John Madden';
+            $id2 = 2;
+            $test_brand = new Brand($name, $id);
+            $test_brand->save();
+            $test_brand2 = new Brand($name2, $id2);
+            $test_brand2->save();
+
+            //Act
+            $result = Brand::getAll();
+
+            //Assert
+            $this->assertEquals([$test_brand, $test_brand2], $result);
+        }
+
     }
 
 ?>
